cd22afb9aadbce7e39989e2b9e016b7b4dba49d2
dblast-x <garublast@gmail.com>
2023-12-03 07:22:48 -0500

    Found algo for the words
    added debug file for next consults
    
    Started with the meaning

--- try/main_word_picker01.py
+++ try/main_word_picker01.py
@@ -31,24 +31,46 @@ def letter_picker(file: str):
 
 
 def word_picker(file: str):
+    count = 0
     if len(file) < 1:
         with open("src/edited_diabolous.txt", "r") as f:
             print("Find the Pattern.")
-            pattern = re.compile(r"(^[A-Z][a-z]+,\s.*)\s")
+            pattern = re.compile(r"(^[\w]+,\s[svtiadj.]+)\s")
+            for line in f:
+                line = line.rstrip()
+                print(line)
+                match = re.findall(pattern, line)
+                if match:
+                    # print(line)  # D
+                    count += 1
+                    print(match, "found!")
+                else:
+                    continue
+        print(count)
+
+
+def meaning_picker(file: str):
+    count = 0
+    if len(file) < 1:
+        with open("src/edited_diabolous.txt", "r") as f:
+            print("Find the Pattern.")
+            pattern = re.compile(r"^[\w]+,\s[svtiadj.]+\s(.+$)")
             for line in f:
                 line = line.rstrip()
                 match = re.findall(pattern, line)
                 if match:
-                    # print(line)  # D
+                    count += 1
                     print(match, "found!")
                 else:
                     continue
+        print(count)
 
 
 def parser_on():
     inp = input("Type in the file or no >> ")
     # letter_picker(inp)
-    word_picker(inp)
+    # word_picker(inp)
+    meaning_picker(inp)
 
 
 if __name__ == "__main__":
