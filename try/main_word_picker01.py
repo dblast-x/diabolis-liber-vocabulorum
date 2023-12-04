189d9a0e8d2bcc3bb2243af278604e4b3b2d734d
dblast-x <garublast@gmail.com>
2023-12-03 19:58:52 -0500

    Developing meaning algo
    more debug files

--- try/main_word_picker01.py
+++ try/main_word_picker01.py
@@ -31,46 +31,45 @@ def letter_picker(file: str):
 
 
 def word_picker(file: str):
-    count = 0
     if len(file) < 1:
         with open("src/edited_diabolous.txt", "r") as f:
             print("Find the Pattern.")
             pattern = re.compile(r"(^[\w]+,\s[svtiadj.]+)\s")
             for line in f:
                 line = line.rstrip()
-                print(line)
                 match = re.findall(pattern, line)
+                print(line)
                 if match:
-                    # print(line)  # D
-                    count += 1
                     print(match, "found!")
                 else:
                     continue
-        print(count)
+                print(line)
 
 
 def meaning_picker(file: str):
-    count = 0
+    """
+    The right algo it's got to be a search from one
+    definition-word to the other definition-word
+    """
     if len(file) < 1:
         with open("src/edited_diabolous.txt", "r") as f:
             print("Find the Pattern.")
-            pattern = re.compile(r"^[\w]+,\s[svtiadj.]+\s(.+$)")
+            pattern = re.compile(r"^[\w]+,\s[svtiadj.]+\s(.+$)", re.DOTALL)  # BUG:
             for line in f:
                 line = line.rstrip()
                 match = re.findall(pattern, line)
                 if match:
-                    count += 1
+                    print(line)
                     print(match, "found!")
                 else:
                     continue
-        print(count)
 
 
 def parser_on():
-    inp = input("Type in the file or no >> ")
+    inp = input("Type in the file, or no >> ")
     # letter_picker(inp)
-    # word_picker(inp)
-    meaning_picker(inp)
+    word_picker(inp)
+    # meaning_picker(inp)
 
 
 if __name__ == "__main__":
