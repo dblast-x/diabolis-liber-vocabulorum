79c7393810d1dede3e02055637d55bf6a91eb893
dblast-x <garublast@gmail.com>
2023-12-05 16:47:54 -0500

    Found a way to extract the words
    Gotta debug first

--- try/t01.py
+++ try/t01.py
@@ -3,6 +3,14 @@ import re
 
 def letter_picker():
     letters = list()
+    """
+     [
+      'A', 'B', 'C', 'D', 'E', 'F',
+      'G', 'H', 'I', 'J', 'K', 'L',
+      'M', 'N', 'O', 'P', 'Q', 'R',
+      'S', 'T', 'U', 'V', 'W', 'Y', 'Z' ->> 25 letters
+     ]
+    """
     with open("src/edited_diabolous.txt", "r") as f:
         print("Find the Letter.")
         pattern = re.compile(r"^[A-Z]\n")
@@ -17,26 +25,35 @@ def letter_picker():
     return letters
 
 
+# BUG: Debug this file, there's and eternal loop.
+# BUG: Debug this file, there is a repetition in the write part
+#  #  DONE!! the text var is got to go blank again
+# FIX: The last letter z is out of boundries
+
+
 def search_words(letters):
     with open("src/edited_diabolous.txt", "r") as f:
         start, end = 0, 1
         for letter in range(0, len(letters)):
             text = """"""
-            with open(f"try/abc/{letters[start]}.txt", "w") as target:
+            with open(f"try/abc/{letters[letter]}.txt", "w") as target:
                 for line in f:
-                    a = re.search(rf"^{start}\n", line)
+                    a = re.search(rf"^{letters[start]}\n", line)
                     if a is not None:
                         print(a)
                         text += line
-                    b = re.search(rf"^{end}\n", line)
+                    b = re.search(rf"^{letters[end]}\n", line)
                     if b is None:
                         text += line
                     else:
                         print(b)
                         break
                     target.write(text)
-            start += letter
-            end += letter
+                    text = """"""
+            start += 1
+            end += 1
 
+
+# print(letter_picker())
 
 search_words(letter_picker())
