b44269dceaac2af8f2b96d979b5c5196ed1964c8
dblast-x <garublast@gmail.com>
2023-12-22 12:56:34 -0500

    Started query process

---
+++ try/query_data.py
@@ -0,0 +1,8 @@
+""" 
+    Steps >>
+      1. Get word as request.
+      2. Open db.
+      3. Search for word.
+        ( if word in db. ->> return definition as response.
+        (( else          ->> return error as typo.
+"""
